309d7091406fe7fb83b3844f0eb006b3c033f680
S3R6104BR3U <sergioabreusousa@gmail.com>
2025-10-14 14:27:24 -0300

    definida pagina padrao

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,4 @@
-//let nome = document.querySelector('#name');
-
-
-// 1. Pegar o ID no final da URL
+// Pegar o ID no final da URL
 const url = window.location.href;
 const id = url.substring(url.lastIndexOf('=') + 1);
 console.log('ID capturado:', id);
@@ -11,12 +8,8 @@ fetch('src/json/dados.json')
   .then(res => res.json())
   .then(data => {
 
-    console.log(data);
-
     const pessoa = data.find(p => p.id === id);
 
-
-    console.log(pessoa)
 
     if (!pessoa) {
       console.warn('Cadastro não encontrado!');
@@ -26,7 +19,6 @@ fetch('src/json/dados.json')
 
     // Exibir primeiro nome
     const primeiroNome = pessoa.nomeCompleto.split(' ')[0];
-    console.log(primeiroNome)
     document.getElementById('photo-profile').src = pessoa.photoProfile;
     document.getElementById('name').textContent = primeiroNome;
 
@@ -35,24 +27,8 @@ fetch('src/json/dados.json')
     document.getElementById('birth').textContent = pessoa.dataNascimento;
     document.getElementById('mae').textContent = pessoa.responsaveis.mae;
     document.getElementById('pai').textContent = pessoa.responsaveis.pai;
-    let phone1 = document.getElementById('phone1');
-    phone1.href = pessoa.contatos[0];
-    phone1.textContent = pessoa.contatos[0];
-    
+    document.getElementById('phone1').textContent = pessoa.contatos[0]
     document.getElementById('phone2').textContent = pessoa.contatos[1];
     document.getElementById('laudo').href = pessoa.linkLaudo;
   })
   .catch(err => console.error(err));
-
-/*
-
-fetch('src/json/dados.json')
-  .then(res => res.json())
-  .then(data => {
-    const abel = data.find(p => p.nomeCompleto === 'Abel Martins Sousa');
-    console.log(abel);
-    const primeiroNome = abel.nomeCompleto.split(' ')[0];
-    console.log(primeiroNome)
-    nome.innerHTML = `${primeiroNome}`
-  });
-//nome.innerHTML = `${abel.nomeCompleto}` */
